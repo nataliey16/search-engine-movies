b0ad1a1cbf6261ed46b914c8676276571a7bf941
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-10-24 15:23:33 -0600

    Added api call to search movie data

--- src/Movies.js
+++ src/Movies.js
@@ -1,40 +1,63 @@
 import React, { useState } from "react";
+import axios from "axios";
+// import Results from "./Results";
 import "./Movies.css";
 
 export default function Movies() {
-  let [movie, setMovie] = useState(null);
+  let [movie, setMovie] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [results, setResults] = useState("");
 
-  function handleMovieSearch(event) {
+  function handleMovieResponse(response) {
+    // console.log(response.data);
+    setResults(response.data.title);
+    setLoaded(true);
+  }
+
+  function handleMovieData() {
+    // https://www.omdbapi.com/
+    let apiKey = "dcf858c8";
+    let apiUrl = ` http://www.omdbapi.com/?t=${movie}&apikey=${apiKey}&`;
+
+    axios.get(apiUrl).then(handleMovieResponse);
+  }
+
+  function handleMovieSubmit(event) {
     event.preventDefault();
-    alert(`${movie}`);
+    handleMovieData();
   }
 
   function handleMovieChange(event) {
-    event.preventDefault();
     setMovie(event.target.value);
   }
 
-  return (
-    <div className="Movies">
-      <form onChange={handleMovieChange}>
-        <div className="row justify-content-center">
-          <div className="col-sm-10">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Search for a movie"
-            ></input>
+  if (loaded) {
+    return (
+      <div className="Movies">
+        <form onChange={handleMovieChange}>
+          <div className="row justify-content-center">
+            <div className="col-sm-10">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search for a movie"
+              ></input>
+            </div>
+            <div className="col-sm-2 ">
+              <input
+                className="btn btn-branding"
+                type="submit"
+                value="search"
+                onClick={handleMovieSubmit}
+              ></input>
+            </div>
           </div>
-          <div className="col-sm-2 ">
-            <input
-              className="btn btn-branding"
-              type="submit"
-              value="search"
-              onClick={handleMovieSearch}
-            ></input>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+        </form>
+        <h1>{results}</h1>
+      </div>
+    );
+  } else {
+    handleMovieData();
+    return <div>Loading...</div>;
+  }
 }
