e7e32e0aa0b2ac6aae87f63b1dc20914d92478ff
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-10-24 13:02:52 -0600

    Added event listeners to form and search button

--- src/Movies.js
+++ src/Movies.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Movies.css";
 
 export default function Movies() {
+  let [movie, setMovie] = useState(null);
+
+  function handleMovieSearch(event) {
+    event.preventDefault();
+    alert(`${movie}`);
+  }
+
+  function handleMovieChange(event) {
+    event.preventDefault();
+    setMovie(event.target.value);
+  }
+
   return (
     <div className="Movies">
-      <div>hello from movies</div>
+      <form onChange={handleMovieChange}>
+        <div className="row justify-content-center">
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search for a movie"
+            ></input>
+          </div>
+          <div className="col-sm-2 ">
+            <input
+              className="btn btn-branding"
+              type="submit"
+              value="search"
+              onClick={handleMovieSearch}
+            ></input>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
